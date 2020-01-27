278c97442d2c3d6e163be14a8757f8bcaebf09ec
Regan Iwadha <reaganiwadha@protonmail.com>
2020-01-27 08:03:47 +0700

    Added ExitHandler

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 'use strict';
 require('dotenv').config();
 
+require('./src/exitHandler');
+
 const debug = require('debug')('app');
 
-const Express = require('express');
-const app = Express();
+//const Express = require('express');
+//const app = Express();
 
 const {expressConfig} = require('./src/config');
 
 const bot = require('./src/bot/bot');
 const lokka = require('./src/lokka/lokka');
 
-app.get('/', (req,res) => res.sendStatus(200));
-app.listen(expressConfig.port, () => debug('Bot started'));
+//app.get('/', (req,res) => res.sendStatus(200));
+//app.listen(expressConfig.port, () => debug('Bot started'));
+
